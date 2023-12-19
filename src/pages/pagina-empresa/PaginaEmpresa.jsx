4aa82ae82b3ef39e88eeab36b9f5cfd71baaafb9
biason franco <franco.biason@gmail.com>
2023-12-19 17:05:06 -0300

    PagNegocio

--- src/pages/pagina-empresa/PaginaEmpresa.jsx
+++ src/pages/pagina-empresa/PaginaEmpresa.jsx
@@ -1,31 +1,79 @@
 import React from "react";
 import "./pagina-empresa.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const mapContainerStyle = {
+  width: "100%",
+  height: "300px", // Ajusta la altura según tus necesidades
+};
+
+const center = {
+  lat: -53.788625,
+  lng: -67.701736,
+};
+
+const options = {
+  disableDefaultUI: true, // Puedes personalizar esto según tus necesidades
+};
 
 const PaginaEmpresa = () => {
+  const imagenesCarrusel = [
+    "/images/portfolio/biasonautomotores.jpeg",
+    "/images/portfolio/biasonautomotores.jpeg",
+    "/images/portfolio/biasonautomotores.jpeg",
+    "/images/portfolio/biasonautomotores.jpeg",
+    "/images/portfolio/biasonautomotores.jpeg",
+    "/images/portfolio/biasonautomotores.jpeg",
+  ];
+
+  const carruselSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+  };
+
+  function CustomPrevArrow(props) {
+    return <div className="custom-arrow custom-prev-arrow" {...props} />;
+  }
+
+  function CustomNextArrow(props) {
+    return <div className="custom-arrow custom-next-arrow" {...props} />;
+  }
+
   return (
     <section className="d-flex background">
       <div className="pagina-empresa-container container">
         {/* Columna 1 */}
         <div className="column-1">
           <div className="perfil-card-element1">
-            <div>
-              <h1>Imagenes Empresa</h1>
+            <div className="logo-nombre-container">
+              <div className="logo-container">
+                <img
+                  src="/images/logo-ba.png"
+                  alt="Logo de la empresa"
+                  className="logo-empresa"
+                />
+              </div>
+              <div className="nombre-slogan-container">
+                <h2>BIASON AUTOMOTORES</h2>
+                <p>Tu lugar de confianza</p>
+              </div>
             </div>
 
-            <div className="logo-container">
-              {/* Aquí va el logo de la empresa */}
-              <img
-                src="/images/logos/logo-vefrek-white.png"
-                alt="Logo de la empresa"
-                className="logo-empresa"
-              />
-            </div>
-            <div className="nombre-slogan-container">
-              {/* Aquí va el nombre de la empresa */}
-              <h2>BIASON AUTOMOTORES</h2>
-              {/* Aquí va el slogan de la empresa */}
-              <p>Slogan de la Empresa</p>
-            </div>
+            <Slider {...carruselSettings}>
+              {imagenesCarrusel.map((imagen, index) => (
+                <div key={index} className="carrusel-item">
+                  <img src={imagen} alt={`Imagen ${index + 1}`} />
+                </div>
+              ))}
+            </Slider>
           </div>
 
           <div className="perfil-card-element1 mt-3">
@@ -40,47 +88,67 @@ const PaginaEmpresa = () => {
 
           <div className="perfil-card-element1 mt-3">
             <div className="reseña-container">
-              {/* Aquí va "Reseña, Calificacion, Comentarios" */}
               <h2>Reseña, Calificación, Comentarios</h2>
             </div>
           </div>
         </div>
+        {/* Fin Columna 1 */}
 
         {/* Columna 2 */}
         <div className="perfil-card-element2">
           <div className="column-2">
             <div className="ubicacion-container">
-              {/* Aquí va el mapa de Google con la ubicación */}
-              {/* Puedes usar un componente de mapa como Google Maps o Mapbox */}
+              <LoadScript googleMapsApiKey="TU_API_KEY">
+                <GoogleMap
+                  mapContainerStyle={mapContainerStyle}
+                  center={center}
+                  zoom={15} // Puedes ajustar el nivel de zoom según tus necesidades
+                  options={options}
+                />
+              </LoadScript>
             </div>
 
             <div className="telefono-container">
-              {/* Aquí va el número de teléfono */}
-              <p>Teléfono: +123456789</p>
+              <p>Teléfono: +54 2966449951</p>
             </div>
+
             <div className="redes-sociales-container">
-              {/* Aquí van los iconos de las redes sociales */}
-              {/* Puedes usar iconos de bibliotecas como Font Awesome */}
-              <span>Redes Sociales: Facebook, Twitter, Instagram, etc.</span>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-facebook"></i>
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-x"></i>
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-youtube"></i>
+              </a>
+              <a href="#" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-linkedin"></i>
+              </a>
+              <a
+                href="enlace_a_instagram"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-instagram"></i>
+              </a>
             </div>
+
             <div className="sitio-web-container">
-              {/* Aquí va el sitio web de la empresa */}
               <p>Sitio Web: www.biasonautomotores.com.ar</p>
             </div>
             <div className="horarios-container">
-              {/* Aquí van los horarios de la empresa */}
               <p>Horarios: Lunes a Viernes: 9:00 AM - 6:00 PM</p>
             </div>
             <div className="reportar-negocio-container">
-              {/* Aquí va el botón "Reportar Negocio" */}
               <button>Reportar Negocio</button>
             </div>
             <div className="contactar-propietario-container">
-              {/* Aquí va el enlace "Contactar a propietario" */}
               <a href="#">Contactar a propietario</a>
             </div>
           </div>
         </div>
+        {/* Fin Columna 2 */}
       </div>
     </section>
   );
