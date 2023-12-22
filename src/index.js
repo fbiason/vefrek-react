c4d3e6aba7573e3c040ff93dfa278037f0b88b23
biason franco <franco.biason@gmail.com>
2023-12-21 23:32:19 -0300

    margen

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { UserProvider } from "./context/userContext";
-import { NextUIProvider } from "@nextui-org/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <UserProvider>
-    <NextUIProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </NextUIProvider>
+    {/*<React.StrictMode>*/}
+    <App />
+    {/* </React.StrictMode>*/}
   </UserProvider>
 );
--- src/pages/publicacion/Publicacion.jsx
+++ src/pages/publicacion/Publicacion.jsx
@@ -6,68 +6,6 @@ import { UserContext } from "../../context/userContext";
 import { swalPopUp } from "../../utils/swal";
 
 const Publicacion = () => {
-<<<<<<< HEAD
-
-    const {userData} = useContext(UserContext);
-
-    const toEmpresa = (e) => {
-        e.preventDefault();
-        if (userData.isLogged) {
-            e.target.parentNode.click()
-        } else {
-            swalPopUp("Ups!", "Tienes que loguerte para puclicar", "info")
-        }
-    }
-
-    return (
-        <section id="hero-publicacion" className="d-flex background">
-            <div className="container" data-aos="fade-up">
-                <div
-                    className="row justify-content-center"
-                    data-aos="fade-up"
-                    data-aos-delay="160"
-                >
-                    <div className="col-xl-6 col-lg-8 logo-publicacion">
-                        <img
-                            src="/images/logos/logo-vefrek-white.png"
-                            alt="Vefrek"
-                            className="vefrek-logo"
-                        />{" "}
-                        <div>&nbsp;</div>
-                        <h2>REALIZA UNA PUBLICACION</h2>
-                        <div>&nbsp;</div>
-                        <h3>¿Qué desea publicar?</h3>
-                    </div>
-                </div>
-
-                <div
-                    className="row gy-4 mt-5 justify-content-center"
-                    data-aos="zoom-in"
-                    data-aos-delay="250"
-                >
-                    <div className="col-xl-2 col-md-4">
-                        <div className="icon-box">
-                            <i className="ri-car-line"></i>
-                            <h3>
-                                <a href="">Vehiculo</a>
-                            </h3>
-                        </div>
-                    </div>
-                    <div className="col-xl-2 col-md-4">
-                        <div className="icon-box">
-                            <i className="ri-building-line"></i>
-                            <h3>
-                                <Link to="/CargaEmpresa">
-                                    <a onClick={toEmpresa}>Empresa</a>
-                                </Link>
-                            </h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-=======
   return (
     <section id="hero-publicacion" className="d-flex background">
       <div className="container" data-aos="fade-up">
@@ -106,7 +44,6 @@ const Publicacion = () => {
       </div>
     </section>
   );
->>>>>>> 7b95a87bdfb03c95d278be465f168c8df24cbd1e
 };
 
 export default Publicacion;
