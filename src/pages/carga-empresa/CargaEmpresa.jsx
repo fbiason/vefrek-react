614ff7f47e10f70a26fe870d3e5c51349863ce52
ArielCorrado <jarielc2012@yahoo.com.ar>
2023-12-22 15:05:13 -0300

    pull 22-12

--- src/pages/carga-empresa/CargaEmpresa.jsx
+++ src/pages/carga-empresa/CargaEmpresa.jsx
@@ -103,7 +103,7 @@ const CargaEmpresa = () => {
     return (
         <div className="perfil-container"> 
             <div className="container">
-                <div className="perfil-card container">
+                <div className="perfil-card container"> 
                     <h2>Carga tu negocio:</h2>
                     <form onSubmit={handleSubmit} className="companyUpdateForm">
                         <div className="form-row">
