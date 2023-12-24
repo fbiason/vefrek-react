9a49e62c9faa8b803e23cc65bb03138b322009cc
biason franco <franco.biason@gmail.com>
2023-12-24 12:54:53 -0300

    CargaNegocio

--- src/pages/prueba-carga/PruebaCarga.jsx
+++ src/pages/prueba-carga/PruebaCarga.jsx
@@ -577,7 +577,7 @@ const PruebaCarga = () => {
             qué las notificaciones que desees recibir.
           </p>
 
-          <div className="mt-10 space-y-10">
+          <div className="mt-3 space-y-10 ">
             <fieldset>
               <div className="mt-6 space-y-6">
                 <div className="relative flex gap-x-3">
@@ -587,31 +587,27 @@ const PruebaCarga = () => {
                       name="Comentarios"
                       type="checkbox"
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                  </div>
-                  <div className="text-sm leading-6">
+                    />{" "}
                     <label
                       htmlFor="comments"
-                      className="font-medium text-gray-900"
+                      className="text-sm leading-6 pl-3 font-medium text-gray-900"
                     >
                       Comentarios
                     </label>
                   </div>
                 </div>
 
-                <div className="relative flex gap-x-3">
+                <div className="relative gap-x-3">
                   <div className="flex h-6 items-center">
                     <input
                       id="comments"
                       name="Comentarios"
                       type="checkbox"
                       className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                  </div>
-                  <div className="text-sm leading-6">
+                    />{" "}
                     <label
                       htmlFor="comments"
-                      className="font-medium text-gray-900"
+                      className="text-sm leading-6 pl-3 font-medium text-gray-900"
                     >
                       Novedades de Vefrek
                     </label>
