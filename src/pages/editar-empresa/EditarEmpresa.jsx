dd1cd82c66d1e02c322b9d902d66a97071003e73
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-01-06 13:40:56 -0300

    egregando editar empresa

--- src/pages/editar-empresa/EditarEmpresa.jsx
+++ src/pages/editar-empresa/EditarEmpresa.jsx
@@ -1,66 +1,70 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "./editar-empresa.css";
 import { SpinnerContext } from "../../context/spinnerContext";
-import { useParams } from "react-router-dom";
-import { findCompany } from "../../utils/apiDb/apiDbAcions";
+import { findCompanys } from "../../utils/apiDb/apiDbAcions";
 import { swalPopUp } from "../../utils/swal";
+import { UserContext } from "../../context/userContext";
 
 const EditarEmpresa = () => {
-  const { showSpinner } = useContext(SpinnerContext);
-  const { name } = useParams();
-  const [companyData, setCompanyData] = useState({ name: "" });
+    const { showSpinner } = useContext(SpinnerContext);
+    const [companysData, setCompanysData] = useState([]);
+    const { userData } = useContext(UserContext);
 
-  const find = async () => {
-    showSpinner(true);
-    const response = await findCompany("name", name, "");
-    if (response.success && response.companyData) {
-      const companyData = response.companyData;
-      setCompanyData({
-        name: companyData.name,
-      });
-    } else if (response.success && !response.companyData) {
-      swalPopUp("Ops", response.message, "warning");
-      showSpinner(false);
-      window.location = "/";
-    } else {
-      swalPopUp("Error", response.message, "error");
-    }
-    showSpinner(false);
-  };
+    const find = async () => {
+        showSpinner(true);
+        const response = await findCompanys(JSON.stringify({"registeremail": userData.email}), "name location");
+        if (response.success && response.companysData) {
+            const companysDataFromDB = response.companysData;
+            const companysDataJSX = companysDataFromDB.map((company) => 
+                <div className="row-editar-empresa flex" key={company._id}>
+                    <div className="col-editar-empresa">
+                        <span>{company.name}</span>
+                    </div>
+                    <div className="col-editar-empresa">
+                        <span>{company.location}</span>
+                    </div>
+                    <div className="col-editar-empresa">
+                        <span role="img" aria-label="Editar">
+                            ✏️ Editar
+                        </span>
+                    </div>
+                    <div className="col-editar-empresa">
+                        <span role="img" aria-label="Pausar">
+                            ⏸️ Pausar
+                        </span>
+                    </div>
+                    <div className="col-editar-empresa">
+                        <span role="img" aria-label="Eliminar">
+                            🗑️ Eliminar
+                        </span>
+                    </div>
+                </div >
+            );
+            setCompanysData(companysDataJSX);
+        } else if (response.success && !response.companysData) {
+            swalPopUp("Ops", response.message, "warning");
+            showSpinner(false);
+            // window.location = "/";
+        } else {
+            swalPopUp("Error", response.message, "error");
+        }
+        showSpinner(false);
+    };
 
-  return (
-    <section className="background">
-      {" "}
-      <div className="editar-empresa-container">
-        <div className="card-editar-empresa">
-          <div className="row-editar-empresa">
-            <div className="col-editar-empresa">
-              <span>{companyData.name}</span>
+    useEffect(() => {
+        if(userData.email) find();
+    }, [userData.email]);
+
+    return (
+        <section className="background">
+            {" "}
+            <div className="editar-empresa-container">
+                <div className="card-editar-empresa">
+                    {companysData}
+                </div>
             </div>
-            <div className="col-editar-empresa">
-              <span>{companyData.url}</span>
-            </div>
-            <div className="col-editar-empresa">
-              <span role="img" aria-label="Editar">
-                ✏️ Editar
-              </span>
-            </div>
-            <div className="col-editar-empresa">
-              <span role="img" aria-label="Pausar">
-                ⏸️ Pausar
-              </span>
-            </div>
-            <div className="col-editar-empresa">
-              <span role="img" aria-label="Eliminar">
-                🗑️ Eliminar
-              </span>
-            </div>
-          </div>
-          <hr style={{ borderColor: "red" }} />
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default EditarEmpresa;
