7b35997b21248b2b7bab673f64fe8db389bbe840
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-01-12 22:32:51 -0300

    cm

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/home/Home";
 import NavBar from "./pages/navbar/NavBar";
 import LoginApp from "./pages/login/Login";
 import { isLogged } from "./utils/auth/isLogged";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { UserContext } from "./context/userContext";
 import Footer from "./pages/footer/Footer";
 import { Navigate } from "react-router-dom";
@@ -22,17 +22,13 @@ import MisEmpresas from "./pages/misEmpresas/MisEmpresas";
 import Contacto from "./pages/contacto/Contacto";
 import LoginUser from "./pages/loginUser/LoginUser";
 import EditarEmpresa from "./pages/editarEmpresa/EditarEmpresa";
-import { BgSpinner } from "./components/spinner/Spinner";
-import NoAutorizado from "./pages/noAutorizado/NoAutorizado";
 
 function App() {
-	const { updateUserData, setShow, userData } = useContext(UserContext);
+	const { updateUserData, setShow, userData, show } = useContext(UserContext);
 	const { spinner } = useContext(SpinnerContext);
-	const [logging, setLogging] = useState(false);
-
+	
 	useEffect(() => {
 		const verifiLog = async () => {
-			setLogging(true);
 			const response = await isLogged();
 			if (response.userData) {
 				updateUserData(response.userData);
@@ -40,7 +36,6 @@ function App() {
 				updateUserData({ email: "", name: "", isLogged: false });
 			}
 			setShow(true);
-			setLogging(false);
 		};
 		verifiLog();
 		// eslint-disable-next-line
@@ -53,62 +48,20 @@ function App() {
 				<NavBar />
 				<Routes>
 					<Route path="/" element={<Home />} />
-					<Route path="/:vefrek_website" element={<PaginaEmpresa />} />
+					{show && <Route path="/:vefrek_website" element={<PaginaEmpresa />} />}
 					<Route path="/login" element={<LoginApp />} />
 					<Route path="/Publicacion" element={<Publicacion />} />
-				
-					{
-						(logging
-							&& <Route path="/perfil" element={<BgSpinner />} />
-						) ||
-						(!logging &&
-							((userData.isLogged && <Route path="/perfil" element={<Perfil />} />)
-								||
-								(!userData.isLogged && <Route path="/perfil" element={<NoAutorizado />} />))
-						)
-					}			
-
-					{
-						(logging
-							&& <Route path="/CargaEmpresa" element={<BgSpinner />} />
-						) ||
-						(!logging &&
-							((userData.isLogged && <Route path="/CargaEmpresa" element={<CargaEmpresa />} />)
-								||
-								(!userData.isLogged && <Route path="/CargaEmpresa" element={<NoAutorizado />} />))
-						)
-					}			
-
-					{!userData.isLogged && <Route path="/loginuser/:token?" element={<LoginUser />} />}
 					<Route path="/Reparacion" element={<Reparacion />} />
 					<Route path="/Venta" element={<Venta />} />
 					<Route path="/OtrosServicios" element={<OtrosServicios />} />
 					<Route path="/Dropdown" element={<Dropdown />} />
 					<Route path="/Contacto" element={<Contacto />} />
-				
-					{
-						(logging
-							&& <Route path="/MisEmpresas" element={<BgSpinner />} />
-						) ||
-						(!logging &&
-							((userData.isLogged && <Route path="/MisEmpresas" element={<MisEmpresas />} />)
-								||
-								(!userData.isLogged && <Route path="/MisEmpresas" element={<NoAutorizado />} />))
-						)
-					}		
-
-					{
-						(logging
-							&& <Route path="/EditarEmpresa/:id" element={<BgSpinner />} />
-						) ||
-						(!logging &&
-							((userData.isLogged && <Route path="/EditarEmpresa/:id" element={<EditarEmpresa />} />)
-								||
-								(!userData.isLogged && <Route path="/EditarEmpresa/:id" element={<NoAutorizado />} />))
-						)
-					}			
-
-					<Route path="*" element={<Navigate to="/" />} />
+					{userData.isLogged && <Route path="/perfil" element={<Perfil />} />}
+					{userData.isLogged && <Route path="/CargaEmpresa" element={<CargaEmpresa />} />}
+					{userData.isLogged && <Route path="/MisEmpresas" element={<MisEmpresas />} />}
+					{userData.isLogged && <Route path="/EditarEmpresa/:id" element={<EditarEmpresa />} />}
+					{!userData.isLogged && <Route path="/loginuser/:token?" element={<LoginUser />} />}
+					{show * <Route path="*" element={<Navigate to="/" />} />}
 				</Routes>
 				<Footer />
 			</NextUIProvider>
