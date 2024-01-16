8be9a9ded2e8383c5fc801142845b186eac8c6fd
biason franco <franco.biason@gmail.com>
2024-01-16 20:54:41 -0300

    G

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,11 +5,8 @@ import Contacto from "../contacto/Contacto";
 import Pcc from "../ppc/Pcc";
 import Negocios from "../negocios/Negocios";
 import Valores from "../valores/Valores";
-import ReactGA from "react-ga";
 
 const Home = () => {
-  ReactGA.initialize("G-J1JT10S65V");
-
   return (
     <>
       <Hero />
