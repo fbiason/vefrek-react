23195c003b35b1a288142dac38b479e2b1ad3fda
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-01-17 01:04:26 -0300

    se convirtio searchbar en componente

---
+++ src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,101 @@
+import "./searchBar.css";
+import { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { findCompanys } from "../../utils/apiDb/apiDbAcions";
+
+export default function SearchBar() {
+
+    const [results, setResults] = useState(null);
+    const companyFullCollection = useRef();
+    const resultsRef = useRef();
+
+    const findData = () => {
+        const resultsCont = document.querySelector(".resultsCont");
+        const inputSearch = document.querySelector(".search_navbar");
+        const inputSearchArr = inputSearch.value.split(" ");
+
+        if (inputSearch.value.length > 1) {
+            const resultsArr = companyFullCollection.current.filter((company) =>
+                inputSearchArr.every((word) =>
+                    JSON.stringify(company).toLowerCase().includes(word.toLowerCase())
+                )
+            );
+            resultsArr.length === 0
+                ? (resultsCont.style.opacity = 0)
+                : (resultsCont.style.opacity = 1);
+            const resultsJSX = resultsArr.map((result) => (
+                <Link
+                    key={result._id}
+                    to={`/${result.vefrek_website}`}
+                    onClick={(e) => {
+                        inputSearch.value = "";
+                        e.target.parentNode.style.opacity = 0;
+                    }}
+                    className="searchResult"
+                >
+                    {`${result.subcategory} - ${result.name} - ${result.location} - ${result.city} - ${result.state}`}
+                </Link>
+            ));
+            setResults(resultsJSX);
+        } else {
+            setResults(null);
+            resultsCont.style.opacity = 0;
+        }
+    };
+
+    const setSearchResultsStyle = () => {
+        setTimeout(() => {
+            const resultsCont = document.querySelector(".resultsCont");
+            if (
+                window.innerHeight > window.innerWidth &&
+                resultsRef.current &&
+                resultsRef.current.length > 0
+            ) {
+                const toLeft = resultsCont.getBoundingClientRect().left;
+                if (toLeft) resultsCont.style.transform = `translateX(-${toLeft}px)`;
+            } else if (
+                window.innerHeight < window.innerWidth &&
+                resultsRef.current &&
+                resultsRef.current.length > 0
+            ) {
+                resultsCont.style.transform = `unset`;
+            }
+        }, 100);
+    };
+
+    useEffect(() => {
+        resultsRef.current = results;
+        setSearchResultsStyle();
+        // eslint-disable-next-line
+    }, [results]);
+
+    const getcompanysFullCollection = async () => {
+        const response = await findCompanys(
+            JSON.stringify({ $or: [{ play: true }, { play: { $exists: false } }] }),
+            "name location city state postal_code phone phone2 website category subcategory description vefrek_website"
+        );
+        if (response.success && response.companysData) {
+            companyFullCollection.current = response.companysData;
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("orientationchange", setSearchResultsStyle);
+        getcompanysFullCollection();
+    }, []);
+
+    return (
+        <div className="searchBarCont">
+            <input
+                onChange={findData}
+                type="text"
+                name="buscar"
+                id="buscar"
+                size="35"
+                placeholder="Buscar..."
+                className="form-control search_navbar"
+            />
+            <div className="resultsCont flex column">{results}</div>
+        </div>
+    )
+}
--- src/pages/navbar/NavBar.jsx
+++ src/pages/navbar/NavBar.jsx
@@ -7,97 +7,23 @@ import Isotope from "isotope-layout";
 import AOS from "aos";
 // import GLightbox from "glightbox";
 import { useNavigate, useLocation } from "react-router-dom";
-import { useContext, useRef, useState } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../context/userContext";
 import Dropdown from "../dropdown/Dropdown";
-import { findCompanys } from "../../utils/apiDb/apiDbAcions";
+import SearchBar from "../../components/searchBar/SearchBar";
 
 const NavBar = () => {
     const navigate = useNavigate();
     const { userData, show } = useContext(UserContext);
-    const [results, setResults] = useState(null);
-    const resultsRef = useRef();
-    const companyFullCollection = useRef();
     const thisLocation = useLocation();
     const [showSearchBar, setShowSearchBar] = useState(true);
 
     useEffect(() => {
-        thisLocation.pathname === "/" ? setShowSearchBar(false) : setShowSearchBar(true);
+        thisLocation.pathname === "/" ? setShowSearchBar(false) : setShowSearchBar(true);       //Oculta la barra de busqueda en home
     }, [thisLocation])
-    
-    const findData = () => {
-        const resultsCont = document.querySelector(".resultsCont");
-        const inputSearch = document.querySelector(".search_navbar");
-        const inputSearchArr = inputSearch.value.split(" ");
-
-        if (inputSearch.value.length > 1) {
-            const resultsArr = companyFullCollection.current.filter((company) =>
-                inputSearchArr.every((word) =>
-                    JSON.stringify(company).toLowerCase().includes(word.toLowerCase())
-                )
-            );
-            resultsArr.length === 0
-                ? (resultsCont.style.opacity = 0)
-                : (resultsCont.style.opacity = 1);
-            const resultsJSX = resultsArr.map((result) => (
-                <Link
-                    to={`/${result.vefrek_website}`}
-                    onClick={(e) => {
-                        inputSearch.value = "";
-                        e.target.parentNode.style.opacity = 0;
-                    }}
-                    className="searchResult"
-                >
-                    {`${result.subcategory} - ${result.name} - ${result.location} - ${result.city} - ${result.state}`}
-                </Link>
-            ));
-            setResults(resultsJSX);
-        } else {
-            setResults(null);
-            resultsCont.style.opacity = 0;
-        }
-    };
-
-    const setSearchResultsStyle = () => {
-        setTimeout(() => {
-            const resultsCont = document.querySelector(".resultsCont");
-            if (
-                window.innerHeight > window.innerWidth &&
-                resultsRef.current &&
-                resultsRef.current.length > 0
-            ) {
-                const toLeft = resultsCont.getBoundingClientRect().left;
-                if (toLeft) resultsCont.style.transform = `translateX(-${toLeft}px)`;
-            } else if (
-                window.innerHeight < window.innerWidth &&
-                resultsRef.current &&
-                resultsRef.current.length > 0
-            ) {
-                resultsCont.style.transform = `unset`;
-            }
-        }, 100);
-    };
-
+        
     useEffect(() => {
-        resultsRef.current = results;
-        setSearchResultsStyle();
-        // eslint-disable-next-line
-    }, [results]);
-
-    const getcompanysFullCollection = async () => {
-        const response = await findCompanys(
-            JSON.stringify({ $or: [{ play: true }, { play: { $exists: false } }] }),
-            "name location city state postal_code phone phone2 website category subcategory description vefrek_website"
-        );
-        if (response.success && response.companysData) {
-            companyFullCollection.current = response.companysData;
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener("orientationchange", setSearchResultsStyle);
-        getcompanysFullCollection();
-
+        
         /***************************** CORRIGE ERROR DE REDIRECCION AL LOGUEARSE CON FACEBOOK -NO BORRAR* */
 
         if (window.location.hash.includes("_=_")) {
@@ -424,18 +350,9 @@ const NavBar = () => {
                     <div className="row align-items-center">
                         {/* Barra de Búsqueda */}
                         { showSearchBar &&
-                        <div className="col-md-4 mb-3 mb-md-0 d-flex justify-content-center searchBarCont">
-                            <input
-                                onChange={findData}
-                                type="text"
-                                name="buscar"
-                                id="buscar"
-                                size="35"
-                                placeholder="Buscar..."
-                                className="form-control search_navbar"
-                            />
-                            <div className="resultsCont flex column">{results}</div>
-                        </div>
+                            <div className="col-md-4 mb-3 mb-md-0 d-flex justify-content-center">
+                                <SearchBar/>
+                            </div>
                         }
 
                         <div className="col-md-4 mb-3 mb-md-0 justify-content-center">
