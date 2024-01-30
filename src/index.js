e3cd0b1fa8d55ceb4ebbc5d0af5292f6628e781c
biason franco <franco.biason@gmail.com>
2024-01-29 22:45:02 -0300

    Informe

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./assets/vendor/bootstrap/css/bootstrap.min.css"
-import "./assets/vendor/bootstrap-icons/bootstrap-icons.css"
-import "./assets/vendor/boxicons/css/boxicons.min.css"
-import "./assets/vendor/glightbox/css/glightbox.min.css"
-import "./assets/vendor/remixicon/remixicon.css"
-import "./assets/vendor/swiper/swiper-bundle.min.css"
+import "./assets/vendor/bootstrap/css/bootstrap.min.css";
+import "./assets/vendor/bootstrap-icons/bootstrap-icons.css";
+import "./assets/vendor/boxicons/css/boxicons.min.css";
+import "./assets/vendor/glightbox/css/glightbox.min.css";
+import "./assets/vendor/remixicon/remixicon.css";
+import "./assets/vendor/swiper/swiper-bundle.min.css";
 import "./index.css";
 import App from "./App";
 import { UserProvider } from "./context/userContext";
@@ -16,7 +16,7 @@ root.render(
   <SpinnerProvider>
     <UserProvider>
       {/*<React.StrictMode>*/}
-      <App /> 
+      <App />
       {/* </React.StrictMode>*/}
     </UserProvider>
   </SpinnerProvider>
