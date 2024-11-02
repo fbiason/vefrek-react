feaa3a8c7b3614658843bf7ce2cedc66eb4627dd
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-11-01 21:23:23 -0300

    cm

--- src/pages/PaginaEmpresa copy.jsx
+++ src/pages/PaginaEmpresa copy.jsx
@@ -380,46 +380,6 @@ const PaginaEmpresa = () => {
         },
     });
 
-    const sendFeedback = async () => {
-        try {
-            if (!userData.isLogged)
-                throw new Error("Debes iniciar sesión para enviar una reseña");
-
-            const hasStars = document
-                .querySelector(".starsFeedback")
-                .querySelector("input[type='radio']:checked");
-            const numberOfStars = hasStars
-                ? parseInt(
-                    document
-                        .querySelector(".starsFeedback")
-                        .querySelector("input[type='radio']:checked")
-                        .getAttribute("weight")
-                )
-                : undefined;
-
-            const companyId = companyDataRef.current._id;
-            const comment = document.querySelector(".commentFeedback").value;
-            showSpinner(true);
-            const response = await addReview(numberOfStars, companyId, comment);
-            if (response.success) {
-                swalPopUp("Acción completada", response.message, "success");
-                document.querySelector(".commentFeedback").value = "";
-                document
-                    .querySelector(".starsFeedback")
-                    .querySelectorAll("input")
-                    .forEach((input) => (input.checked = false));
-            } else if (!response.success && !response.message.includes("Error")) {
-                swalPopUp("Ops!", response.message, "warning");
-            } else if (!response.success && response.message.includes("Error")) {
-                swalPopUp("Ops!", response.message, "error");
-            }
-        } catch (err) {
-            swalPopUp("Ops!", err.message, "error");
-        }
-        showSpinner(false);
-        find();
-    };
-
     useEffect(() => {
         (async () => {
             //Campo "timestamp" -> Es la ultima vez que se entro al anuncio
@@ -963,7 +923,6 @@ const PaginaEmpresa = () => {
                                 <div className="mt-3" onClick={report}>
                                     <button className="reportar">Reportar Negocio</button>
                                 </div>
-
                                 {
                                     companyData._id &&
                                     <Review
@@ -972,75 +931,7 @@ const PaginaEmpresa = () => {
                                         newReview={true}
                                         userData={userData}
                                     />
-                                }            
-
-                                {/* <div className="max-w-lg shadow-md mt-4">
-                                    <form action="" className="w-full p-2">
-                                        <div className="mb-2">
-                                            <label
-                                                htmlFor="comment"
-                                                className="text-gray-600 w-full text-center"
-                                            >
-                                                Deja tu comentario sobre la empresa
-                                            </label>
-                                            <textarea
-                                                className="w-full h-20 p-2 border rounded focus:outline-none focus:ring-gray-300 focus:ring-1 commentFeedback"
-                                                name="comment"
-                                                placeholder=""
-                                            ></textarea>
-                                        </div>
-
-                                        <div className="container r-star">
-                                            <div className="feedback">
-                                                <div className="rating starsFeedback">
-                                                    <input
-                                                        type="radio"
-                                                        name="rating"
-                                                        id="rating-5"
-                                                        weight="5"
-                                                    />
-                                                    <label htmlFor="rating-5"></label>
-                                                    <input
-                                                        type="radio"
-                                                        name="rating"
-                                                        id="rating-4"
-                                                        weight="4"
-                                                    />
-                                                    <label htmlFor="rating-4"></label>
-                                                    <input
-                                                        type="radio"
-                                                        name="rating"
-                                                        id="rating-3"
-                                                        weight="3"
-                                                    />
-                                                    <label htmlFor="rating-3"></label>
-                                                    <input
-                                                        type="radio"
-                                                        name="rating"
-                                                        id="rating-2"
-                                                        weight="2"
-                                                    />
-                                                    <label htmlFor="rating-2"></label>
-                                                    <input
-                                                        type="radio"
-                                                        name="rating"
-                                                        id="rating-1"
-                                                        weight="1"
-                                                    />
-                                                    <label htmlFor="rating-1"></label>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <button
-                                            className="px-3 py-2 text-sm text-blue-100 bg-blue-600 rounded"
-                                            onClick={sendFeedback}
-                                            type="button"
-                                        >
-                                            Comentar
-                                        </button>
-                                    </form>
-                                </div> */}
+                                }           
                             </div>
                         </div>
                     </div>
